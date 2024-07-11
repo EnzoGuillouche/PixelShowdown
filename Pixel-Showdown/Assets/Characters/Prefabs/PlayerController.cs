7cbeaba01a5a90ce0e8237ea9af3be0c089be95e
Lucas Megnan <lucas.megnan@algosup.com>
2024-07-11 15:29:55 +0200

    player jouable à la manette mais sprite marche pas ?, fonctionne pour le rebind qui sera ajouter plus tard

--- Pixel-Showdown/Assets/Characters/Prefabs/PlayerController.cs
+++ Pixel-Showdown/Assets/Characters/Prefabs/PlayerController.cs
@@ -1,101 +1,72 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Dynamic;
 using UnityEngine;
-using UnityEngine.InputSystem;
-
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    Vector2 moveInput;
-    public float walkSpeed = 10f;
+    private Rigidbody2D rb;
 
-    private bool _isMoving = false;
-    public bool isMoving { 
-        get
-        {
-            return _isMoving;
-        }
-        private set
-        {
-            _isMoving = value;
-            animator.SetBool("isMoving", value);
-        }
-    }
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public float dashDistance = 5f;
+    public float dashDuration = 0.2f;
+    public float crouchSpeedMultiplier = 0.5f;
 
-    private bool _isJumping = false;
-    public bool isJumping
-    {
-        get
-        {
-            return _isJumping;
-        }
-        private set
-        {
-            _isJumping = value;
-            animator.SetBool("isJumping", value);
-        }
-    }
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask whatIsGround;
 
-    public bool _isFacingRight = true;
-    public bool isFacingRight {
-        get
-        {
-            return _isFacingRight;
-        }
-        private set
-        {
-            if (_isFacingRight != value) {
-                transform.localScale *= new Vector2(-1, 1);
-            }
-
-            _isFacingRight = value;
-        }
-    }
-
-    Rigidbody2D rb;
-    Animator animator;
-
-    private void Awake()
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
     }
 
-    void Start()
+    private void Update()
     {
-        
-    }
+        // Récupération des inputs à chaque frame
+        float moveInput = Input.GetAxis("Horizontal");
 
-    void Update()
-    {
-        
-    }
+        // Gestion du mouvement horizontal
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
 
-    private void FixedUpdate()
-    {
-        rb.velocity = new Vector2(moveInput.x * walkSpeed, rb.velocity.y);
-    }
+        // Vérification si le joueur est au sol
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
 
-    public void onMove(InputAction.CallbackContext context)
-    {
-        moveInput = context.ReadValue<Vector2>();
-        isMoving = moveInput != Vector2.zero;
-
-        SetFacingDirection(moveInput);
-    }
-
-    public void onJump()
-    {
-        isJumping = !isJumping;
-    }
-
-    private void SetFacingDirection(Vector2 moveInput)
-    {
-        if (moveInput.x > 0 && !isFacingRight) {
-            isFacingRight = true;
-        } else if (moveInput.x < 0 && isFacingRight) {
-            isFacingRight = false;
+        // Gestion du saut
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+
+        // Gestion du dash
+        if (Input.GetButtonDown("Dash"))
+        {
+            StartCoroutine(Dash());
+        }
+
+        // Autres actions comme l'attaque, etc.
+        // Exemple : if (Input.GetButtonDown("Att1")) { ... }
+    }
+
+    // Coroutine pour gérer le dash
+    private IEnumerator Dash()
+    {
+        // Désactiver la gravité pendant le dash (optionnel)
+        rb.gravityScale = 0f;
+
+        // Calcul de la direction du dash
+        Vector2 dashDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized * dashDistance;
+
+        // Appliquer le dash pendant la durée définie
+        float dashTimer = 0f;
+        while (dashTimer < dashDuration)
+        {
+            rb.velocity = dashDirection / dashDuration;
+            dashTimer += Time.deltaTime;
+            yield return null;
+        }
+
+        // Réactiver la gravité après le dash (optionnel)
+        rb.gravityScale = 1f;
     }
 }
